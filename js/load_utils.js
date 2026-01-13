5ac08df80d4b1937e2db5c73dc458e1cafa00bf7
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-01-13 14:55:10 -0500

    put back and torso back

--- js/load_utils.js
+++ js/load_utils.js
@@ -16,10 +16,10 @@ const OBJ_THOLDS = {
   "palm tree": 0,
   "human face": 0,
   "human hand": 0,
-  "naked human back": 1,
+  "naked human back": 0,
   "naked human breast": 1,
   "naked human buttocks": 1,
-  "naked human torso": 1,
+  "naked human torso": 0,
 };
 
 async function fetchData(mUrl) {
